e7c68f83cad720e1741cbf66c634aee046a234db
omodtung <dothedung131@gmail.com>
2024-06-28 21:57:41 +0700

    rewrite for suitable with frontend

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -33,11 +33,11 @@ export class CompaniesController {
   @Get()
   @ResponseMessage('Fetch List Company with Paginate')
   findAll(
-    @Query('page') currentPage: string,
-    @Query('limit') limit: string,
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
     @Query() qs: string,
   ) {
-    return this.companiesService.findAll(+currentPage, +limit, qs);
+    return this.companiesService. findAll(+currentPage, +limit, qs);
   }
 
   @Get(':id')
