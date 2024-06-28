4a10480d9b9f5d7afa1f451046053d7893c4a0aa
omodtung <dothedung131@gmail.com>
2024-06-28 21:55:09 +0700

    Extra UseGuard for User can Use @User() and write endpoint Logout  in stateless controller

--- src/stateless/stateless.controller.ts
+++ src/stateless/stateless.controller.ts
@@ -18,8 +18,9 @@ import cookieParser from 'cookie-parser';
 import { Request as RequestExpress } from 'express';
 // import { Request } from 'express';
 import { IUser } from '@/users/users.interface';
+import { use } from 'passport';
 // import { Request as Req1 } from 'express';
-@Controller('stateless')
+@Controller('auth')
 export class StatelessController {
   constructor(private statelessService: StatelessService) {}
   @UseGuards(LocalAuthGuard)
@@ -27,7 +28,7 @@ export class StatelessController {
   @ResponseMessage('User login')
   // @Res({passthrough:true}) dong nay khi data duoc tra ve chi co server co the doc duoc
   async login(@Request() req, @Res({ passthrough: true }) response: Response) {
-    return this.statelessService.login(req.user, response);
+    return await this.statelessService.login(req.user, response);
   }
 
   @UseGuards(JwtAuthGuard)
@@ -49,6 +50,7 @@ export class StatelessController {
   //fix for this take data user back
 
   @ResponseMessage('Get user information')
+  @UseGuards(JwtAuthGuard)
   @Get('/account')
   handleGetAccount(@User() user: IUser) {
     return { user };
@@ -62,5 +64,16 @@ export class StatelessController {
   ) {
     const refresh_token = request.cookies['refresh_token'];
     return this.statelessService.processNewToken(refresh_token, response);
+  }
+  //fix bug Logiut user : underfine
+  @ResponseMessage('LogOut user')
+  @UseGuards(JwtAuthGuard)
+  @Post('/logout')
+  handleLogout(
+    @Res({ passthrough: true }) response: Response,
+    @User() user: IUser,
+  ) {
+    // const  Find = user._id;
+    return this.statelessService.logout(response, user);
   }
 }
